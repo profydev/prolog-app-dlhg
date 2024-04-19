60082db3314270f24d4edc6bf9d7a13269f874da
dlhg <gragg.drew@gmail.com>
2024-04-19 18:22:09 -0400

    Update project status names and colors to match API docs and figma design

--- cypress/e2e/project-list.cy.ts
+++ cypress/e2e/project-list.cy.ts
@@ -1,4 +1,5 @@
 import capitalize from "lodash/capitalize";
+import { ProjectStatus } from "@api/projects.types";
 import mockProjects from "../fixtures/projects.json";
 
 describe("Project List", () => {
@@ -36,6 +37,45 @@ describe("Project List", () => {
           cy.wrap($el)
             .find("a")
             .should("have.attr", "href", "/dashboard/issues");
+        });
+    });
+    it("provides the correct color for each project status", () => {
+      cy.get("main")
+        .find("li")
+        .each(($el, index) => {
+          const statusColors: { [index: string]: string } = {
+            [ProjectStatus.info]: "rgb(2, 122, 72)",
+            [ProjectStatus.warning]: "rgb(181, 71, 8)",
+            [ProjectStatus.error]: "rgb(180, 35, 24)",
+          };
+
+          // retrieve the element containing the status badge within the list item
+          const element = cy.wrap($el).find("div[class*='badge_container']");
+
+          // get the project's status from the mock data array corresponding to the current list item
+          const status = mockProjects[index].status;
+
+          // check correct color for status
+          element.should("have.css", "color", statusColors[status]);
+        });
+    });
+
+    it("provides the correct text for each project status", () => {
+      cy.get("main")
+        .find("li")
+        .each(($el, index) => {
+          const statusTexts: { [index: string]: string } = {
+            [ProjectStatus.info]: "stable",
+            [ProjectStatus.warning]: "warning",
+            [ProjectStatus.error]: "Critical",
+          };
+
+          // retrieve the element containing the status badge within the list item
+          const element = cy.wrap($el).find("div[class*='badge_container']");
+          // get the project's status from the mock data array corresponding to the current list item
+          const status = mockProjects[index].status;
+          // check correct text for status
+          element.invoke("text").should("eq", capitalize(statusTexts[status]));
         });
     });
   });
